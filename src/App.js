4a32896b89325714bf4f25548840a95c6eea41dd
Barry <Barry-B15@users.noreply.github.com>
2020-09-11 00:23:29 +0900

    feat: add api, add state, useEffect, update UI

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import Recipe from "./Recipe";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+    const APP_ID = 'f5d94d31';
+    const API_KEY = "50dd58ce94210228fac942b4902dae09";
+
+    // def a state to handle the response data from the api
+    const [recipes, setRecipes] = useState([]);
+
+    //const [counter, setCounter] = useState(0); // def counter and set it to 0
+
+    useEffect(() => {
+        // call get recipe
+        getRecipes();
+    }, []);
+
+    // def an async func to get recipe
+    const getRecipes = async() => {
+        const recipeURL = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${API_KEY}`;
+        const response = await fetch(recipeURL);
+
+        const data = await response.json();
+        console.log(data.hits);
+        setRecipes(data.hits);
+    }
+
+    return ( <
+        div className = "App" >
+        <
+        form className = "search-form" >
+        <
+        input className = "search-bar"
+        type = "text" / >
+
+        <
+        button className = "search-button"
+        type = "submit" > Search < /button>   < /
+        form >
+
+        {
+            recipes.map(recipe => ( <
+                Recipe title = { recipe.recipe.label }
+                calories = { recipe.recipe.calories }
+                image = { recipe.recipe.image }
+                / >
+            ))
+        } <
+        /div>
+    );
+
+
 }
 
 export default App;
