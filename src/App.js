9a322a033a0a847c7515fa49e5dee09eaa46167b
Barry <Barry-B15@users.noreply.github.com>
2020-09-12 00:15:42 +0900

    feat: search and styling

--- src/App.js
+++ src/App.js
@@ -10,16 +10,21 @@ const App = () => {
     // def a state to handle the response data from the api
     const [recipes, setRecipes] = useState([]);
 
+    const [search, setSearch] = useState(''); // create a state for the search
+
+    // state for query entered by user
+    const [query, setQuery] = useState('chicken'); // set to "chicken" since that is what we have as the query in our api url
+
     //const [counter, setCounter] = useState(0); // def counter and set it to 0
 
     useEffect(() => {
         // call get recipe
         getRecipes();
-    }, []);
+    }, [query]);
 
     // def an async func to get recipe
     const getRecipes = async() => {
-        const recipeURL = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${API_KEY}`;
+        const recipeURL = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${API_KEY}`; // change 'chicken' to the 'query', our new state
         const response = await fetch(recipeURL);
 
         const data = await response.json();
@@ -27,27 +32,51 @@ const App = () => {
         setRecipes(data.hits);
     }
 
+    // def a function to run when changes happens
+    const updateSearch = e => {
+        setSearch(e.target.value);
+        //console.log(search);
+    }
+
+    //Def getSearch to get the search query
+    const getSearch = e => {
+        e.preventDefault(); // prevent the page refresh
+        setQuery(search); // setQuery to whatever we have in our search to get the finished text
+        // set this getSearch to onSubmit in the <form> 
+        setSearch(''); // set search bak to null to clear the search box when done
+    }
+
     return ( <
         div className = "App" >
         <
-        form className = "search-form" >
+        form onSubmit = { getSearch }
+        className = "search-form" >
         <
         input className = "search-bar"
-        type = "text" / >
+        type = "text"
+        value = { search }
+        onChange = { updateSearch }
+        / >
 
         <
         button className = "search-button"
         type = "submit" > Search < /button>   < /
         form >
 
+        <
+        div className = "recipes" >
+
         {
             recipes.map(recipe => ( <
-                Recipe title = { recipe.recipe.label }
+                Recipe key = { recipe.recipe.label }
+                title = { recipe.recipe.label }
                 calories = { recipe.recipe.calories }
                 image = { recipe.recipe.image }
+                ingredients = { recipe.recipe.ingredients }
                 / >
             ))
         } <
+        /div>  <
         /div>
     );
 
--- src/Recipe.js
+++ src/Recipe.js
@@ -1,12 +1,20 @@
 import React from "react";
+import style from './recipe.module.css'; // import the css module
 
-const Recipe = ({ title, calories, image }) => {
+const Recipe = ({ title, calories, image, ingredients }) => {
     return ( <
-        div >
+        div className = { style.recipe } >
         <
-        h1 > { title } < /h1> <
-        p > { calories } < /p> <
-        img src = { image }
+        h1 > { title } < /h1>     <
+        ol > {
+            ingredients.map(ingredient => ( <
+                li > { ingredient.text } < /li>
+            ))
+        } <
+        /ol>  <
+        p > { calories } < /p>  <
+        img className = { style.image }
+        src = { image }
         alt = "" / >
 
         <
